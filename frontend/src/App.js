d5a3f2f64bb1bace244fbcd78bf3785ea972954f
sharad paradhi <sharad.paradhi@qicpic.in>
2025-01-07 15:21:21 +0530

    this is test

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      this is the testing purpose;
+      
+    </div>
+  );
+}
+
+export default App;
